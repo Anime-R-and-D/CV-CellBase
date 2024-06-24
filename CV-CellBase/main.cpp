921bfe12522498674f84bb1534de59440f0f0d82
takeshi.okuya <20336848+takeshi-okuya@users.noreply.github.com>
2024-06-24 18:04:03 +0900

    Refactoring: CharacterCellProcessing

--- CV-CellBase/main.cpp
+++ CV-CellBase/main.cpp
@@ -1,5 +1,82 @@
 #include "Filter.hpp"
 
+Mat characterCellProcessing(Mat srcImg) {
+	vector clothesColors = {
+		Vec3b(111, 105, 161),
+		Vec3b(144, 160, 130),
+		Vec3b(163, 168, 165),
+		Vec3b(150, 155, 156),
+	};
+
+	vector hairColors1 = {
+		Vec3b(41, 38, 40),
+		Vec3b(97, 57, 70)
+	};
+
+	vector hairColors2 = {
+		Vec3b(2, 2, 1),
+		Vec3b(44, 17, 10)
+	};
+
+	vector eyeColors = {
+		Vec3b(28, 9, 11),
+		Vec3b(112, 72, 87)
+	};
+
+	vector targetColorsList = { clothesColors, hairColors1, hairColors2, eyeColors };
+
+	Mat layer_1 = applyFilters(
+		srcImg, {
+			make_shared<::CellBlur>(20.0f, 21, targetColorsList),
+		});
+
+	Mat layer_2 = applyFilters(
+		srcImg, {
+			make_shared<::CellBlur>(20.0f, 21, targetColorsList),
+			make_shared<LineRemover>(Vec3b(4,2,10), Vec3b(255,255,255), 100),
+		});
+
+	Mat layer_3 = applyFilters(
+		srcImg, {
+			make_shared<LineOnly>(),
+		});
+
+	Mat layer_1_2 = applyLayersWithAlpha(layer_1, layer_2, 0.7);
+	Mat layer_1_2_3 = applyLayersWithAlpha(layer_1_2, layer_3, 0.3);
+
+	return layer_1_2_3;
+}
+
+void characterCellProcessingMovie(const string& srcImgsPathPattern, const string& dstMoviePath) {
+	vector<cv::String> srcImgPaths;
+	vector<Mat> dstImgs;
+	cv::glob(srcImgsPathPattern, srcImgPaths, true);
+
+	for (const string& srcImgPath : srcImgPaths) {
+		Mat srcImg = imread(srcImgPath);
+		if (srcImg.empty()) {
+			throw "No image found! " + srcImgPath;
+		}
+
+		Mat dstImg = characterCellProcessing(srcImg);
+		dstImgs.push_back(dstImg);
+		imshow("CharacterCellProcessingMovie", dstImg);
+		waitKey(1);
+	}
+
+	int fourcc = cv::VideoWriter::fourcc('M', 'J', 'P', 'G');
+	cv::VideoWriter writer(dstMoviePath, fourcc, 12, dstImgs[0].size(), true);
+	if (!writer.isOpened()) {
+		throw "writer not opened: " + dstMoviePath;
+	}
+
+	for (const Mat& dstImg : dstImgs) {
+		writer.write(dstImg);
+	}
+
+	writer.release();
+}
+
 Mat chalkFilter(Mat srcImage) {
 	auto colorLine = applyFilters(
 		srcImage, {
@@ -30,28 +107,11 @@ static void onMouse(int event, int x, int y, int f, void* param) {
 		auto pixel = image->at<uchar>(y, x);
 		cout << static_cast<int>(pixel) << endl;
 	}
-};
+}
 
 int main()
 {
 	string imagePath = "src.png";
-	string moviePath = "movie_test/*.png";
-
-
-	vector<cv::String> fn;
-	vector<cv::Mat> data;
-	cv::glob(moviePath, fn, true);
-
-	for (size_t k = 0; k < fn.size(); k++)
-	{
-
-		cv::Mat im = cv::imread(fn[k]);
-		if (im.empty()) continue;
-
-		data.push_back(im);
-	}
-
-
 	Mat srcImage = imread(imagePath);
 	if (srcImage.data == NULL)
 	{
@@ -59,125 +119,14 @@ int main()
 	}
 	cv::imshow("Source", srcImage);
 
-	vector clothesColors = {
-		Vec3b(111, 105, 161),
-		Vec3b(144, 160, 130),
-		Vec3b(163, 168, 165),
-		Vec3b(150, 155, 156),
-	};
-
-	vector hairColors1 = {
-		Vec3b(41, 38, 40),
-		Vec3b(97, 57, 70) };
-
-	vector hairColors2 = {
-		Vec3b(2, 2, 1),
-		Vec3b(44, 17, 10) };
-
-	vector eyeColors = {
-		Vec3b(28, 9, 11),
-		Vec3b(112, 72, 87) };
-
-	vector targetColorsList = { clothesColors, hairColors1, hairColors2, eyeColors };
-
-	vector<cv::Mat> newData;
-	for (Mat img : data) {
-
-		Mat layer1 = applyFilters(
-			img, {
-
-				//
-			   // make_shared<::SobelAbsXY>(),
-		  make_shared<::CellBlur>(20.0f, 21, targetColorsList),
-		  //  make_shared<::LineOnly>(),
-	  /*make_shared<::CellBlur>(20.0f, 21, targetColorsList),
-	  make_shared<::GaussianBlur>(10.0f, 11),*/
-			});
-
-		Mat layer2 = applyFilters(
-			img, {
-
-				//
-			   // make_shared<::SobelAbsXY>(),
-				make_shared<::CellBlur>(20.0f, 21, targetColorsList),
-				make_shared<LineRemover>(Vec3b(4,2,10), Vec3b(255,255,255), 100),
-				/*make_shared<::CellBlur>(20.0f, 21, targetColorsList),
-				make_shared<::GaussianBlur>(10.0f, 11),*/
-			});
-		Mat layer3 = applyFilters(
-			img, {
-
-			make_shared<LineOnly>(),
-			});
-		Mat result1 = applyLayersWithAlpha(layer1, layer2, 0.7);
-		Mat finalResult = applyLayersWithAlpha(result1, layer3, 0.3);
-		newData.push_back(finalResult);
-
-	}
-
-	/*for (Mat img : newData) {
-		imshow("newData", img);
-		waitKey(0);
-	}*/
-
-	int fourcc = cv::VideoWriter::fourcc('M', 'J', 'P', 'G');
-
-	
-	cv::VideoWriter writer("results.avi", fourcc, 12, cv::Size(newData.at(0).cols, newData.at(0).rows), true);
-	
-	if (!writer.isOpened()) {
-		cout << "writer not opened" << endl;
-	}
-
-	//std::cout << "ttest" << endl;
-		//imshow("new results", newData);
-	for (int i = 0; i < newData.size(); i++) {
-		writer.write(newData.at(i));
-
-		
-
-
-		
-
-}
-
-	writer.release();
-
-	//Mat layer1 = applyFilters(
-	//	srcImage, {
-	//				  
-	//				  //
-	//				 // make_shared<::SobelAbsXY>(),
-	//			make_shared<::CellBlur>(20.0f, 21, targetColorsList),
-	//				//  make_shared<::LineOnly>(),
-	//			/*make_shared<::CellBlur>(20.0f, 21, targetColorsList),
-	//			make_shared<::GaussianBlur>(10.0f, 11),*/
-	//			  });
-
-	//Mat layer2 = applyFilters(
-	//	srcImage, {
-
-	//		//
-	//	   // make_shared<::SobelAbsXY>(),
-	//		make_shared<::CellBlur>(20.0f, 21, targetColorsList),
-	//		make_shared<LineRemover>(Vec3b(4,2,10), Vec3b(255,255,255), 100),
-	//		/*make_shared<::CellBlur>(20.0f, 21, targetColorsList),
-	//		make_shared<::GaussianBlur>(10.0f, 11),*/
-	//	});
-	//Mat layer3 = applyFilters(
-	//	srcImage, {
-
-	//	make_shared<LineOnly>(),
-	//	});
-
-	//cv::imshow("Image2", dstImage1);
-	//Mat dstImage2 = applyLayersWithAlpha(dstImage1, dstImage, 0.1);
-
 	// imshow("AveragingBlur", AveragingBlur(3, 3).apply(srcImage));
 	// imshow("GaussianBlur", ::GaussianBlur(2.0f, 5).apply(srcImage));
 	// imshow("SobelX", SobelX().apply(srcImage));
 	// imshow("SobelY", SobelY().apply(srcImage));
 	// imshow("SobelAbsXY", SobelAbsXY().apply(srcImage));
+
+	// imshow("CharacterCellProcessing", characterCellProcessing(srcImage));
+	characterCellProcessingMovie("movie_test/*.png", "results.avi");
 
 	// auto chalkImage = chalkFilter(srcImage);
 	// imshow("ChalkFilter", chalkImage);
